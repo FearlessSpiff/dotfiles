603aac936f6b5fd81211a770ef162094d82b5c2c
FearlessSpiff <fearless.spiff@me.com>
2024-02-19 22:20:21 +0100

    move lazygit plugin to git-config

--- .config/nvim/lua/plugins/git-config.lua
+++ .config/nvim/lua/plugins/git-config.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>lg', ':LazyGit <CR>')
+    end
+  },
+  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
