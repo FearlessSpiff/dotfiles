6c8eb46401bda9d77910c60f900b034615eaa789
FearlessSpiff <fearless.spiff@me.com>
2024-02-20 17:21:32 +0100

    add bash lsp and formatter and format all under hyprland

--- .config/hypr/cliphist.sh
+++ .config/hypr/cliphist.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 case $1 in
-    d) cliphist list | rofi -dmenu -replace | cliphist delete
-       ;;
+d)
+	cliphist list | rofi -dmenu -replace | cliphist delete
+	;;
 
-    w) if [ `echo -e "Clear\nCancel" | rofi -dmenu` == "Clear" ] ; then
-            cliphist wipe
-       fi
-       ;;
+w)
+	if [ $(echo -e "Clear\nCancel" | rofi -dmenu) == "Clear" ]; then
+		cliphist wipe
+	fi
+	;;
 
-    *) cliphist list | rofi -dmenu -replace | cliphist decode | wl-copy
-       ;;
+*)
+	cliphist list | rofi -dmenu -replace | cliphist decode | wl-copy
+	;;
 esac
--- .config/hypr/waybar/scripts/toggle-leftbar.sh
+++ .config/hypr/waybar/scripts/toggle-leftbar.sh
@@ -3,7 +3,7 @@
 LEFTBAR_PID="$(pgrep -f '.*waybar/left.*')"
 
 if [[ -n $LEFTBAR_PID ]]; then
-  kill -s SIGTERM $LEFTBAR_PID
+	kill -s SIGTERM $LEFTBAR_PID
 else
-  /usr/bin/waybar -c ~/.config/hypr/waybar/left/waybar-conf.json -s ~/.config/hypr/waybar/left/style.css &
+	/usr/bin/waybar -c ~/.config/hypr/waybar/left/waybar-conf.json -s ~/.config/hypr/waybar/left/style.css &
 fi
