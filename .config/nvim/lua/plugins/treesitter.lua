81b4b85ccfbc154972bc756adbbb7f28fef2c0d3
FearlessSpiff <fearless.spiff@me.com>
2024-02-20 09:38:05 +0100

    add none-ls and some refinements

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "java", "lua", "vim", "vimdoc", "javascript", "html", "css", "typescript" },
+      auto_install = true,
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
