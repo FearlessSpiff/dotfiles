c8149dd1e7d6809687ea4eda894be2662d34b78b
FearlessSpiff <fearless.spiff@me.com>
2024-02-22 12:47:43 +0100

    add line numbering

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set nowrap")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
 
 -- visual-multi plugin in multicursor.lua
 vim.cmd("let g:VM_maps = {}")
