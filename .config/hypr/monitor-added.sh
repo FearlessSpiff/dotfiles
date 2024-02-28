ccd5853bf946641ebdbcc19170bcffdf499b7178
FearlessSpiff <fearless.spiff@me.com>
2024-02-28 12:31:10 +0100

    add listener for monitors (does not work)

---
+++ .config/hypr/monitor-added.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# for usage see https://github.com/coffebar/hyprland-monitor-attached
+# disable laptop monitor on external monitor connect
+hyprctl keyword monitor eDP-1,disable
---
+++ .config/hypr/monitor-removed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# for usage see https://github.com/coffebar/hyprland-monitor-attached
+# enable laptop monitor on external monitor disconnect
+hyprctl keyword monitor eDP-1,preferred,auto,2
