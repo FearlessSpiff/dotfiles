b3bcb07db718809c71df5e72f87bfddc26784475
FearlessSpiff <fearless.spiff@me.com>
2024-03-01 21:22:44 +0100

    refine and align git signs bindings with neo tree

--- .config/nvim/lua/plugins/git-config.lua
+++ .config/nvim/lua/plugins/git-config.lua
@@ -12,10 +12,12 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gc", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
       vim.keymap.set("n", "<leader>gu", ":Gitsigns reset_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gh", ":Gitsigns stage_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>ga", ":Gitsigns stage_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<CR>", {})
     end
   },
   {
