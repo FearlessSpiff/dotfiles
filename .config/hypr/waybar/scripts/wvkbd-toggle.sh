961f10c16aaccdb366b759c30dab7cfcd3a25b73
FearlessSpiff <fearless.spiff@me.com>
2024-03-19 21:03:12 +0100

    switch to wvkbd desktop layout

--- .config/hypr/waybar/scripts/wvkbd-toggle.sh
+++ .config/hypr/waybar/scripts/wvkbd-toggle.sh
@@ -2,10 +2,10 @@
 
 # "on-click": "killall -34 wvkbd-mobintl",
 
-WVKBD_PID="$(pgrep -f '.*wvkbd-mobintl.*')"
+WVKBD_PID="$(pgrep -f '.*wvkbd-desktop.*')"
 
 if [[ -n $WVKBD_PID ]]; then
-	killall wvkbd-mobintl
+	killall wvkbd-desktop
 else
-	/usr/local/bin/wvkbd-mobintl -L 280 &
+	/usr/local/bin/wvkbd-desktop &
 fi
