22a8419ff8f0854afea5c5de099a3f1eb548f066
FearlessSpiff <fearless.spiff@me.com>
2024-03-21 18:50:48 +0100

    add nvim plugin to better see indentation

--- .config/nvim/lua/plugins/eye-candy.lua
+++ .config/nvim/lua/plugins/eye-candy.lua
@@ -1,13 +1,14 @@
 return {
-	{
-		"brenoprata10/nvim-highlight-colors",
-		config = function()
-			require("nvim-highlight-colors").setup({})
-		end,
-	},
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup({})
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 }
