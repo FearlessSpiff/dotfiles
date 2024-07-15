443b6b785a20b9864bebd990048ddcb07bb25112
FearlessSpiff <fearless.spiff@me.com>
2024-07-15 20:44:38 +0200

    remove java.lua as it interferes with lsp-config (check that later) and add which-key

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = true })
+      end,
+      desc = "Global Keymaps (which-key)",
+    },
+  }
+}
