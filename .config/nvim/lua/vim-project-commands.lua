b83feabf737d499648717d5e9fc4cb20572116d0
FearlessSpiff <fearless.spiff@me.com>
2024-07-16 14:14:46 +0200

    switch to LazyVim from own config

--- .config/nvim/lua/vim-project-commands.lua
+++
@@ -1,2 +0,0 @@
--- waybar helpers
-vim.keymap.set("n", "<leader>rw", ":!killall waybar && sleep 1 && waybar -c /home/steph/.config/hypr/waybar/top/waybar-conf.json -s /home/steph/.config/hypr/waybar/top/style.css &<CR>", {})
