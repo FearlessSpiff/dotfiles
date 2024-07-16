073476795b1f0e13781b252cca1848a3bdc0e3a5
FearlessSpiff <fearless.spiff@me.com>
2024-07-16 21:27:32 +0200

    add arrow mapping to navigate between windows

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- Move to window using the <ctrl> Arrow keys
+map("n", "<A-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<A-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<A-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<A-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
