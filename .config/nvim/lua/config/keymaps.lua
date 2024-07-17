bb7b710cb5a8d2e7bdb31ce99151a36211cc4f42
FearlessSpiff <fearless.spiff@me.com>
2024-07-17 13:33:13 +0200

    add arrow mapping to navigate buffers

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,7 @@ map("n", "<A-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 map("n", "<A-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<A-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<A-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- buffers
+map("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
