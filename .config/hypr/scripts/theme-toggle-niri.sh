1e517529c9abeb2213d7a2c85893e525aefb85c4
FearlessSpiff <fearless.spiff@me.com>
2025-09-30 17:00:23 +0200

    Add config for Niri

---
+++ .config/hypr/scripts/theme-toggle-niri.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IS_DARK_MODE="$(grep style-dark.css ~/.config/hypr/waybar/scripts/topbar-toggle.sh)"
+
+echo IS_DARK_MODE
+if [[ -n $IS_DARK_MODE ]]; then
+  sed -i 's/black-skull.jpeg/clouds.jpeg/g' ~/.config/hypr/hyprpaper.conf
+  sed -i 's/style-dark.css/style.css/g' ~/.config/hypr/waybar/scripts/topbar-toggle.sh
+  sed -i 's/style-dark.css/style.css/g' ~/.config/hypr/waybar/scripts/leftbar-toggle.sh
+else
+  sed -i 's/clouds.jpeg/black-skull.jpeg/g' ~/.config/hypr/hyprpaper.conf
+  sed -i 's/style.css/style-dark.css/g' ~/.config/hypr/waybar/scripts/topbar-toggle.sh
+  sed -i 's/style.css/style-dark.css/g' ~/.config/hypr/waybar/scripts/leftbar-toggle.sh
+fi
+
+killall waybar
+~/.config/hypr/waybar/scripts/topbar-toggle.sh
+
+killall hyprpaper
+sleep 1
+hyprpaper &
--- .config/hypr/waybar/scripts/topbar-toggle.sh
+++ .config/hypr/waybar/scripts/topbar-toggle.sh
@@ -2,7 +2,7 @@
 
 WAYBAR_PID="$(pgrep -f '.*waybar/(left|top).*')"
 if [[ -n $WAYBAR_PID ]]; then
-	killall waybar
+  killall waybar
 else
-	/usr/bin/waybar -c ~/.config/hypr/waybar/top/waybar-conf.json -s ~/.config/hypr/waybar/top/style.css &
+  /usr/bin/waybar -c ~/.config/hypr/waybar/top-niri/waybar-conf.json -s ~/.config/hypr/waybar/top-niri/style.css &
 fi
