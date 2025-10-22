1c3287e07d5594bd20b1cb0123cf8dda251e8824
FearlessSpiff <fearless.spiff@me.com>
2025-10-22 15:44:21 +0200

    Add brighness notification and remove from waybar

---
+++ .config/hypr/scripts/change-brightness.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+case $1 in
+inc)
+  out=$(brightnessctl -m --class=backlight set +10%)
+  ;;
+dec)
+  out=$(brightnessctl -m --class=backlight set 10%-)
+  ;;
+esac
+
+#echo "$out"
+array=(${(s:,:)out})
+brightnessPct=$array[4]
+echo "$brightnessPct"
+if [[ "$brightnessPct" == "0%" ]]; then
+    brightnessPct="1%"
+    brightnessctl --class=backlight set 1%
+fi
+dunstify -t 2000 'Brightness' -h int:value:${brightnessPct}
